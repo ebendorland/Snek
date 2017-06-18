00d515cc97fc1bb05661f556ac2a85cb2bdcd98a
KCHETTY <kchetty416@gmail.com>
2017-06-18 06:36:48 +0200

    working lib

---
+++ ncurses/ncurses.cpp
@@ -0,0 +1,75 @@
+#include "ncurses.hpp"
+
+extern "C" dynamic_libs *create()
+{
+    return (new ncurses);
+}
+
+extern "C" void destroy(dynamic_libs *obj)
+{
+    endwin();
+    delete obj;
+}
+
+void ncurses::init(unsigned int &maxX,unsigned int &maxY)
+{
+    this->max_y = maxY;
+    this->max_x = maxX;
+    initscr();
+	start_color();
+	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_GREEN, COLOR_BLACK);
+	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(4, COLOR_BLUE, COLOR_BLACK);
+	init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
+	init_pair(6, COLOR_CYAN, COLOR_BLACK);
+	raw();
+	noecho();
+    nodelay(stdscr, TRUE);
+	keypad(stdscr, TRUE);
+	//getmaxyx(stdscr, this->max_y, this->max_x);
+	//if ((maxX < 20 + 4) || (maxY < 20))
+    //{
+	//	clear();
+	//	endwin();
+	//	std::cout << "This screen has rows " << maxX << " and " << maxY <<  "columns. resize window" << std::endl;
+	//}
+}
+
+void ncurses::render(char **map)
+{
+    clear();
+    for (int tmp_y = 0; tmp_y < (this->max_y + 2); tmp_y++)
+    {
+        for (int tmp_x = 0; tmp_x < this->max_x + 2; tmp_x++)
+        {
+            printw("%c", map[tmp_y][tmp_x]);
+        }
+        printw("\n");
+    }
+    refresh();
+}
+
+int ncurses::input(int &dir)
+{
+    switch (getch())
+    {
+        case KEY_UP:
+            if (dir != 3)
+                return (1);
+            break ;
+        case KEY_RIGHT:
+            if (dir != 4)
+                return (2);
+            break ;
+        case KEY_DOWN:
+            if (dir != 1)
+                return (3);
+            break ;
+        case KEY_LEFT:
+            if (dir != 2)
+                return (4);
+            break ;
+    }
+    return (dir);
+}
